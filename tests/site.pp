869aaabe84bf5ff737d21669a88527f61d8e2efc
Jeff McCune <jeff@puppetlabs.com>
2011-03-22 16:32:09 -0700

    Whitespace only change to tests

--- tests/site.pp
+++ tests/site.pp
@@ -1,13 +1,13 @@
 node default {
 
-	$rabbitmq_plugins = [ 'amqp_client-2.3.1.ez', 'rabbit_stomp-2.3.1.ez' ]
+  $rabbitmq_plugins = [ 'amqp_client-2.3.1.ez', 'rabbit_stomp-2.3.1.ez' ]
 
   class { 'rabbitmq':
     config => template('rabbitmq/rabbitmq.conf'),
   }
-	class { 'rabbitmq::service':
+  class { 'rabbitmq::service':
     ensure => running,
-	}
+  }
 
   # Required for MCollective
   rabbitmq::plugin { $rabbitmq_plugins:
