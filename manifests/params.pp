9379611f4e3b249fb0c36240be79a83d316214e6
Ashley Penney <ashley.penney@puppetlabs.com>
2013-07-07 14:35:31 -0400

    Minor fixes to syntax so this works under 2.6.

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,12 +6,13 @@ class rabbitmq::params {
 
   case $::osfamily {
     'Debian': {
-      if $config_mirrored_queues {
-        $mirrored_queues_pkg_name = 'rabbitmq-server_2.8.7-1_all.deb',
-        $mirrored_queues_pkg_url  = 'http://www.rabbitmq.com/releases/rabbitmq-server/v2.8.7/',
+      if $::config_mirrored_queues {
+        $mirrored_queues_pkg_name = 'rabbitmq-server_2.8.7-1_all.deb'
+        $mirrored_queues_pkg_url  = 'http://www.rabbitmq.com/releases/rabbitmq-server/v2.8.7/'
         $erlang_pkg_name          = 'erlang-nox'
       }
     }
+    default: { }
   }
 
 }
