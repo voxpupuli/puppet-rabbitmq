39af9eec7f6e233b0d911e726e87f2ecbc18fccf
Ashley Penney <ashley.penney@puppetlabs.com>
2013-07-16 17:39:05 -0400

    Add framework for rspec-system tests.

---
+++ spec/system/class_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper_system'
+
+describe "rabbitmq class:" do
+  context 'should run successfully' do
+    pp="
+      class { 'erlang': }
+      class { 'rabbitmq': }
+      Class['erlang'] -> Class['rabbitmq']
+    "
+
+    context puppet_apply(pp) do
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should_not == 1 }
+      its(:refresh) { should be_nil }
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should be_zero }
+    end
+  end
+
+  context 'service_ensure => stopped:' do
+    pp = "class { 'rabbitmq': service_ensure => stopped }"
+
+    context puppet_apply(pp) do
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should_not == 1 }
+      its(:refresh) { should be_nil }
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should be_zero }
+    end
+  end
+
+  context 'service_ensure => running:' do
+    pp = "class { 'rabbitmq': service_ensure => running }"
+
+    context puppet_apply(pp) do |r|
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should_not == 1 }
+      its(:refresh) { should be_nil }
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should be_zero }
+    end
+  end
+end
