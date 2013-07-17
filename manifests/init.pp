8ff99679a402e1d129037fa987174f6bf6531191
Ashley Penney <ashley.penney@puppetlabs.com>
2013-07-17 16:19:11 -0400

    Add the parameters and variables needed for rabbitmq::config.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,55 @@
+#
 class rabbitmq(
-  $admin_enable     = $rabbitmq::params::admin_enable,
-  $erlang_manage    = $rabbitmq::params::erlang_manage,
-  $management_port  = $rabbitmq::params::management_port,
-  $package_ensure   = $rabbitmq::params::package_ensure,
-  $package_name     = $rabbitmq::params::package_name,
-  $package_provider = $rabbitmq::params::package_provider,
-  $package_source   = $rabbitmq::params::package_source,
-  $service_ensure   = $rabbitmq::params::service_ensure,
-  $service_manage   = $rabbitmq::params::service_manage,
-  $service_name     = $rabbitmq::params::service_name,
-  $version          = $rabbitmq::params::version,
+  $admin_enable             = $rabbitmq::params::admin_enable,
+  $cluster_disk_nodes       = $rabbitmq::params::cluster_disk_nodes,
+  $cluster_node_type        = $rabbitmq::params::cluster_node_type,
+  $cluster_nodes            = $rabbitmq::params::cluster_nodes,
+  $config                   = $rabbitmq::params::config,
+  $config_cluster           = $rabbitmq::params::config_cluster,
+  $config_mirrored_queues   = $rabbitmq::params::config_mirrored_queues,
+  $config_path              = $rabbitmq::params::config_path,
+  $config_stomp             = $rabbitmq::params::config_stomp,
+  $delete_guest_user        = $rabbitmq::params::delete_guest_user,
+  $env_config               = $rabbitmq::params::env_config,
+  $env_config_path          = $rabbitmq::params::env_config_path,
+  $erlang_cookie            = $rabbitmq::params::erlang_cookie,
+  $erlang_manage            = $rabbitmq::params::erlang_manage,
+  $manage_service           = $rabbitmq::params::manage_service,
+  $management_port          = $rabbitmq::params::management_port,
+  $node_ip_address          = $rabbitmq::params::node_ip_address,
+  $package_ensure           = $rabbitmq::params::package_ensure,
+  $package_name             = $rabbitmq::params::package_name,
+  $package_provider         = $rabbitmq::params::package_provider,
+  $package_source           = $rabbitmq::params::package_source,
+  $plugin_dir               = $rabbitmq::params::plugin_dir,
+  $port                     = $rabbitmq::params::port,
+  $service_ensure           = $rabbitmq::params::service_ensure,
+  $service_manage           = $rabbitmq::params::service_manage,
+  $service_name             = $rabbitmq::params::service_name,
+  $stomp_port               = $rabbitmq::params::stomp_port,
+  $version                  = $rabbitmq::params::version,
+  $wipe_db_on_cookie_change = $rabbitmq::params::wipe_db_on_cookie_change,
 ) inherits rabbitmq::params {
+
+  # Validate parameters.
+  validate_bool($config_cluster)
+  validate_bool($config_mirrored_queues)
+  validate_bool($config_stomp)
+  validate_bool($delete_guest_user)
+  validate_bool($manage_service)
+  validate_re($port, '\d+')
+  validate_re($stomp_port, '\d+')
+
+  # Handle deprecated option.
+  if $cluster_disk_nodes {
+    notify { 'cluster_disk_nodes':
+      message => 'WARNING: The cluster_disk_nodes is deprecated.
+       Use cluster_nodes instead.',
+    }
+    $cluster_nodes_real = $cluster_disk_nodes
+  } else {
+    $cluster_nodes_real = $cluster_nodes
+  }
 
   if $erlang_manage {
     include '::erlang'
@@ -18,7 +57,7 @@ class rabbitmq(
   }
 
   include '::rabbitmq::install'
-  include '::rabbitmq::server'
+  include '::rabbitmq::config'
   include '::rabbitmq::service'
 
   case $::osfamily {
@@ -49,7 +88,7 @@ class rabbitmq(
   anchor { 'rabbitmq::end': }
 
   Anchor['rabbitmq::begin'] -> Class['::rabbitmq::install']
-    -> Class['::rabbitmq::server'] ~> Class['::rabbitmq::service']
+    -> Class['::rabbitmq::config'] ~> Class['::rabbitmq::service']
     -> Anchor['rabbitmq::end']
 
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -4,13 +4,6 @@
 #
 class rabbitmq::params {
 
-  $admin_enable    = true
-  $erlang_manage   = false
-  $management_port = '15672'
-  $service_ensure  = 'running'
-  $service_manage  = true
-  $service_name    = 'rabbitmq-server'
-
   case $::osfamily {
     'Debian': {
       $package_ensure   = 'installed'
@@ -32,5 +25,32 @@ class rabbitmq::params {
       fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
     }
   }
+
+  #install
+  $admin_enable             = true
+  $erlang_manage            = false
+  $management_port          = '15672'
+  $service_ensure           = 'running'
+  $service_manage           = true
+  $service_name             = 'rabbitmq-server'
+  #config
+  $cluster_disk_nodes       = false
+  $cluster_node_type        = 'disc'
+  $cluster_nodes            = []
+  $config                   = 'rabbitmq/rabbitmq.config'
+  $config_cluster           = false
+  $config_mirrored_queues   = false
+  $config_path              = '/etc/rabbitmq/rabbitmq.conf'
+  $config_stomp             = false
+  $delete_guest_user        = false
+  $env_config               = 'rabbitmq/rabbitmq-env.conf.erb'
+  $env_config_path          = '/etc/rabbitmq/rabbitmq-env.conf'
+  $erlang_cookie            = 'EOKOWXQREETZSHFNTPEY'
+  $manage_service           = true
+  $node_ip_address          = 'UNSET'
+  $plugin_dir               = "/usr/lib/rabbitmq/lib/rabbitmq_server-${version}/plugins"
+  $port                     = '5672'
+  $stomp_port               = '6163'
+  $wipe_db_on_cookie_change = false
 
 }
