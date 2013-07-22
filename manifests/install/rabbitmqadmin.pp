a91cecd9a627940d437f6f5fc2d3b483c6d2f1fb
Bryan Wyatt <bwyatt@clickmotive.com>
2013-07-22 13:41:14 -0500

    Typo fix and replacing more strings with variable

--- manifests/install/rabbitmqadmin.pp
+++ manifests/install/rabbitmqadmin.pp
@@ -3,11 +3,11 @@ class rabbitmq::install::rabbitmqadmin(
 ) inherits rabbitmq {
 
   $rabbitmqadmin_url  = "http://localhost:${management_port}/cli/rabbitmqadmin"
-  $rabbitmqadmin_path = "/var/lib/rabbitmq/rabbitmqadmi"
+  $rabbitmqadmin_path = "/var/lib/rabbitmq/rabbitmqadmin"
   exec { 'Download rabbitmqadmin':
     command => "bash -c 'curl ${rabbitmqadmin_url} -o ${rabbitmqadmin_path} || wget ${rabbitmqadmin_url} -O ${rabbitmqadmin_path}'",
     path    => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
-    creates => '/var/lib/rabbitmq/rabbitmqadmin',
+    creates => $rabbitmqadmin_path,
     require => [
       Class['rabbitmq::service'],
       Rabbitmq_plugin['rabbitmq_management']
@@ -17,7 +17,7 @@ class rabbitmq::install::rabbitmqadmin(
   file { '/usr/local/bin/rabbitmqadmin':
     owner   => 'root',
     group   => 'root',
-    source  => '/var/lib/rabbitmq/rabbitmqadmin',
+    source  => $rabbitmqadmin_path,
     mode    => '0755',
     require => Exec['Download rabbitmqadmin'],
   }
