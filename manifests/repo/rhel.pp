64ee74bfa1eeb50ce0995d08dac3a6f8547ec41f
Jarl Stefansson <jarl@sdelements.com>
2014-04-10 14:24:48 -0400

    Rename exec for fetching gpg key

--- manifests/repo/rhel.pp
+++ manifests/repo/rhel.pp
@@ -4,7 +4,8 @@ class rabbitmq::repo::rhel {
 
   Class['rabbitmq::repo::rhel'] -> Package<| title == 'rabbitmq-server' |>
 
-  exec { "rpm --import ${package_gpg_key}":
+  exec { 'fetch_rabbitmq_gpg_key':
+    command => "rpm --import ${package_gpg_key}",
     path   => ['/bin','/usr/bin','/sbin','/usr/sbin'],
     onlyif => 'test `rpm -qa | grep gpg-pubkey-056e8e56-468e43f2 | wc -l` -eq 0',
   }
