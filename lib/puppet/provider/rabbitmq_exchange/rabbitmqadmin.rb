b7694ea17ec7e2e6bc5a9ebeae9b4069562b3e50
David Gillies <david.gillies@bigcommerce.com>
2014-11-11 09:44:44 +1100

    add -q switch to rabbitmqctl calls in rabbitmq_exchange provider

--- lib/puppet/provider/rabbitmq_exchange/rabbitmqadmin.rb
+++ lib/puppet/provider/rabbitmq_exchange/rabbitmqadmin.rb
@@ -24,7 +24,7 @@ Puppet::Type.type(:rabbitmq_exchange).provide(:rabbitmqadmin) do
 
   def self.all_vhosts
     vhosts = []
-    parse_command(rabbitmqctl('list_vhosts')).collect do |vhost|
+    parse_command(rabbitmqctl('-q', 'list_vhosts')).collect do |vhost|
         vhosts.push(vhost)
     end
     vhosts
@@ -32,7 +32,7 @@ Puppet::Type.type(:rabbitmq_exchange).provide(:rabbitmqadmin) do
 
   def self.all_exchanges(vhost)
     exchanges = []
-    parse_command(rabbitmqctl('list_exchanges', '-p', vhost, 'name', 'type'))
+    parse_command(rabbitmqctl('-q', 'list_exchanges', '-p', vhost, 'name', 'type'))
   end
 
   def self.parse_command(cmd_output)
