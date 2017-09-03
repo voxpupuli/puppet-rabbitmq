0026d7a5d181b2a9dfae0f9da3f1faa27457b7a7
Alexander Fisher <alex@linfratech.co.uk>
2017-09-03 13:03:57 +0100

    Rubocop: Fix Layout/CommentIndentation & Layout/ExtraSpacing

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -1,7 +1,7 @@
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
-                                                    # Original fact sources:
-add_custom_fact :puppetversion, Puppet.version      # Facter, but excluded from rspec-puppet-facts
-add_custom_fact :staging_http_get, ''               # puppet-staging
-add_custom_fact :rabbitmq_version, '3.6.1'          # puppet-rabbitmq
-add_custom_fact :erl_ssl_path, '/usr/lib64/erlang/lib/ssl-7.3.3.1/ebin'          # puppet-rabbitmq
+#                                                                       # Original fact sources:
+add_custom_fact :puppetversion, Puppet.version                          # Facter, but excluded from rspec-puppet-facts
+add_custom_fact :staging_http_get, ''                                   # puppet-staging
+add_custom_fact :rabbitmq_version, '3.6.1'                              # puppet-rabbitmq
+add_custom_fact :erl_ssl_path, '/usr/lib64/erlang/lib/ssl-7.3.3.1/ebin' # puppet-rabbitmq
