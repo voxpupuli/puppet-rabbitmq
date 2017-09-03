4755a437700614f7f7772a43a76040d56c64fa7e
Alexander Fisher <alex@linfratech.co.uk>
2017-09-03 13:13:03 +0100

    Rubocop: Fix Style/IfInsideElse
    We don't support puppet 3 anymore, so most of the code can just go.

--- lib/puppet/provider/rabbitmq_plugin/rabbitmqplugins.rb
+++ lib/puppet/provider/rabbitmq_plugin/rabbitmqplugins.rb
@@ -1,21 +1,10 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'rabbitmqctl'))
 Puppet::Type.type(:rabbitmq_plugin).provide(:rabbitmqplugins, parent: Puppet::Provider::Rabbitmqctl) do
-  if Puppet::PUPPETVERSION.to_f < 3
-    if Facter.value(:osfamily) == 'RedHat'
-      commands rabbitmqplugins: '/usr/lib/rabbitmq/bin/rabbitmq-plugins'
-    else
-      commands rabbitmqplugins: 'rabbitmq-plugins'
-    end
+  case Facter.value(:osfamily)
+  when 'RedHat'
+    has_command(:rabbitmqplugins, '/usr/lib/rabbitmq/bin/rabbitmq-plugins') { environment HOME: '/tmp' }
   else
-    if Facter.value(:osfamily) == 'RedHat'
-      has_command(:rabbitmqplugins, '/usr/lib/rabbitmq/bin/rabbitmq-plugins') do
-        environment HOME: '/tmp'
-      end
-    else
-      has_command(:rabbitmqplugins, 'rabbitmq-plugins') do
-        environment HOME: '/tmp'
-      end
-    end
+    has_command(:rabbitmqplugins, 'rabbitmq-plugins') { environment HOME: '/tmp' }
   end
 
   defaultfor feature: :posix
