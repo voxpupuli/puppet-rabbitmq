a2ba372601e7efa4ed4cb480807f0c4714a61a58
Leo Antunes <leo@costela.net>
2017-09-05 16:21:35 +0200

    couple of fixes for erl_ssl_path
    - with_env got dropped in facter >=3, so stop using it.
    - only call gsub if actually run; This should avoid "undefined method
      `gsub!' for false:FalseClass"
    - add simple test

--- lib/facter/erl_ssl_path.rb
+++ lib/facter/erl_ssl_path.rb
@@ -4,13 +4,10 @@
 # [1] https://www.rabbitmq.com/clustering-ssl.html
 Facter.add('erl_ssl_path') do
   setcode do
-    data = false
     if Facter::Util::Resolution.which('erl')
-      Facter::Util::Resolution.with_env('HOME' => '/root') do
-        data = Facter::Core::Execution.execute("erl -eval 'io:format(\"~p\", [code:lib_dir(ssl, ebin)]),halt().' -noshell")
-      end
+      data = Facter::Core::Execution.execute("erl -eval 'io:format(\"~p\", [code:lib_dir(ssl, ebin)]),halt().' -noshell")
+      # erl returns the string with quotes, strip them off
+      data.gsub!(%r{\A"|"\Z}, '')
     end
-    # erl returns the string with quotes, strip them off
-    data.gsub!(%r{\A"|"\Z}, '')
   end
 end
