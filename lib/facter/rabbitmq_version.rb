98118c4a18bdc72cfb692f59f7aeef4bed9af5ac
William Yardley <wby@axs.com>
2017-09-06 14:40:00 -0700

    Fix for #614 (avoid error when rabbitmqctl is not present), and update spec syntax

--- lib/facter/rabbitmq_version.rb
+++ lib/facter/rabbitmq_version.rb
@@ -1,6 +1,6 @@
 Facter.add(:rabbitmq_version) do
   setcode do
-    if Facter::Core::Execution.which('rabbitmqadmin')
+    if Facter::Util::Resolution.which('rabbitmqadmin')
       rabbitmq_version = Facter::Core::Execution.execute('rabbitmqadmin --version 2>&1')
       %r{^rabbitmqadmin ([\w\.]+)}.match(rabbitmq_version)[1]
     end
