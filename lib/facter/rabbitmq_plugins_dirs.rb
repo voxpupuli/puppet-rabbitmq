ce7d4cee2400a4cef5e17927e3ca8200c431e195
Jiri Stransky <jistr@redhat.com>
2019-03-25 17:57:15 +0100

    Don't crash on rabbitmq_plugins_dirs fact if rabbitmqctl is not present
    Before trying to access the contents of a regexp match, see if the
    match was successful. On machines without rabbitmqctl this match will
    return `nil`, so we can't index it with `[1]`. When that's the case,
    the whole rabbitmq_plugins_dirs fact value will now be `nil`, instead
    of throwing an exception.
    
    Fixes: #783

--- lib/facter/rabbitmq_plugins_dirs.rb
+++ lib/facter/rabbitmq_plugins_dirs.rb
@@ -2,8 +2,8 @@ Facter.add(:rabbitmq_plugins_dirs) do
   setcode do
     if Facter::Util::Resolution.which('rabbitmqctl')
       rabbitmq_pluginsdirs_env = Facter::Core::Execution.execute("rabbitmqctl eval 'application:get_env(rabbit, plugins_dir).'")
-      rabbitmq_plugins_dirs = %r{^\{ok\,\"(\/.+\/\w+)}.match(rabbitmq_pluginsdirs_env)[1]
-      rabbitmq_plugins_dirs.split(':')
+      rabbitmq_plugins_dirs_match = %r{^\{ok\,\"(\/.+\/\w+)}.match(rabbitmq_pluginsdirs_env)
+      rabbitmq_plugins_dirs_match[1].split(':') if rabbitmq_plugins_dirs_match
     end
   end
 end
