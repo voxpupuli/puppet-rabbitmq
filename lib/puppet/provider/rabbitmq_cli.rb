63fee2c701e78f61d0612738d849176ad862729c
Bradley Bishop <bishopbm1@gmail.com>
2021-03-16 23:57:22 -0400

    CLI Environment Fixes (#876)
    Fixes issue where the local encoding is not correct by adding the correct 
    LC_ALL values to the environment.

--- lib/puppet/provider/rabbitmq_cli.rb
+++ lib/puppet/provider/rabbitmq_cli.rb
@@ -25,7 +25,7 @@ class Puppet::Provider::RabbitmqCli < Puppet::Provider
 
   def self.home_tmp_command(name, path)
     has_command name, path do
-      environment HOME: '/tmp'
+      environment HOME: '/tmp', LC_ALL: 'en_US.UTF-8'
     end
   end
 
