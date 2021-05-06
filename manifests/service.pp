0514e4a7bdaa1ab400941d92a370f03fd73dca6f
Christoph Maser <christoph.maser+github@gmail.com>
2021-05-06 17:01:44 +0200

    Compatibility with camptocamp/systemd 3.x (#886)
    * call systemd::systemctl::daemon_reload only if it is defined
    
    systemd::systemctl::daemon_reload was removed in camptocamp-systemd
    3.0.0

--- manifests/service.pp
+++ manifests/service.pp
@@ -22,8 +22,7 @@ class rabbitmq::service (
       hasrestart => true,
       name       => $service_name,
     }
-
-    if $facts['systemd'] {
+    if $facts['systemd'] and defined(Class['systemd::systemctl::daemon_reload']) {
       Class['systemd::systemctl::daemon_reload'] -> Service['rabbitmq-server']
     }
   }
